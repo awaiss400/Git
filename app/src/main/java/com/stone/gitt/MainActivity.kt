025f5ef47293cbdd3d1496116b3fd5cc98ffe42b
awaiss400 <testingbscs@gmail.com>
2022-09-07 18:24:57 +0500

    feature and master commit

--- app/src/main/java/com/stone/gitt/MainActivity.kt
+++ app/src/main/java/com/stone/gitt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("feature commit")
         println("feature second commit")
+        println("feature second commit")
         println("feature third commit")
     }
 }
