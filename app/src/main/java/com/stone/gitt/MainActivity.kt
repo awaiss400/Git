f88c7e2f63da13da8fdd4106415e17831d3c2600
awaiss400 <testingbscs@gmail.com>
2022-09-07 18:21:22 +0500

    second commit

--- app/src/main/java/com/stone/gitt/MainActivity.kt
+++ app/src/main/java/com/stone/gitt/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 println("first commit ever")
+println("second commit ever")
     }
 
 }
