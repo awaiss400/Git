c4995bfc3412af8679dd9870c388899047e81dca
awaiss400 <testingbscs@gmail.com>
2022-09-07 18:21:49 +0500

    third commit

--- app/src/main/java/com/stone/gitt/MainActivity.kt
+++ app/src/main/java/com/stone/gitt/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 println("first commit ever")
 println("second commit ever")
+println("thirrd commit ever")
     }
 
 }
